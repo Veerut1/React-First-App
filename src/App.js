0aa16cfe07bbbe4479392ee9d7a6a0a785d09391
unknown <Virupaksha.VT@yethi.in>
2020-04-18 17:10:08 +0530

    First commit.

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Switch,Route} from 'react-router-dom';
+
+
+import './App.scss';
+import HomePage from './pages/homepage/home.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
